9e39a85c62d218621736fd1fdca485f40a460930
AndreiPetoukhov1986 <oziglena@mail.ru>
2023-05-08 22:48:39 +0300

    second commit

---
+++ src/main/java/com/example/hw211/controller/StoreController.java
@@ -0,0 +1,33 @@
+package com.example.hw211.controller;
+
+import com.example.hw211.Item;
+import com.example.hw211.service.StoreService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Set;
+
+@RestController
+@RequestMapping("/order")
+public class StoreController {
+
+    private final StoreService storeService;
+
+    public StoreController(StoreService storeService) {
+        this.storeService = storeService;
+    }
+
+    @GetMapping("/add")
+    public void add(@RequestParam("id") Set<Integer> ids) {
+        storeService.add(ids);
+    }
+
+    @GetMapping("/get")
+    public List<Item> get() {
+        return storeService.get();
+    }
+
+}
